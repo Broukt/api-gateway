1a587716a0164a6f3be3b04f0439469f65b25180
Jairo Calcina <j.calcina.v@gmail.com>
2025-06-10 10:22:42 -0400

    feat: add auth service client

---
+++ src/clients/authServiceClient.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const authServiceClient = axios.create({
+  baseURL: process.env.AUTH_SERVICE_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default authServiceClient;
