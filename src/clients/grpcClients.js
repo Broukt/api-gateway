7492e33c6fa4cb1916f83436f4a302594b2f4ecb
Jairo Calcina <j.calcina.v@gmail.com>
2025-06-10 10:22:18 -0400

    feat: add grpc clients

---
+++ src/clients/grpcClients.js
@@ -0,0 +1,27 @@
+const { loadProto } = require("../utils/loadProto.js");
+const { credentials } = require("@grpc/grpc-js");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const loadClients = (app) => {
+  const usersProto = loadProto("users");
+  app.locals.usersClient = new usersProto.UserService(
+    process.env.USERS_SERVICE_URL,
+    credentials.createInsecure()
+  );
+
+  const playlistsProto = loadProto("playlists");
+  app.locals.playlistsClient = new playlistsProto.PlaylistService(
+    process.env.PLAYLISTS_SERVICE_URL,
+    credentials.createInsecure()
+  );
+
+  const emailsProto = loadProto("emails");
+  app.locals.emailsClient = new emailsProto.EmailService(
+    process.env.EMAILS_SERVICE_URL,
+    credentials.createInsecure()
+  );
+};
+
+module.exports = loadClients;
