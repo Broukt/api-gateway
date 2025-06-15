5e1ad8c78c0c47eec91fdb3180173e2b4583e9a8
Luchardiles <luiis_rapero@hotmail.cl>
2025-06-15 18:09:26 -0400

    feat: added permisions of remaining microservices

--- src/config/permissions.js
+++ src/config/permissions.js
@@ -16,4 +16,23 @@ module.exports = {
   "GET /listas-reproduccion": ["Administrador", "Cliente"],
   "GET /listas-reproduccion/:id/videos": ["Administrador", "Cliente"],
   "DELETE /listas-reproduccion/:id": ["Administrador", "Cliente"],
+
+  "POST /facturas": ["Administrador"],
+  "GET /facturas/:id": ["Administrador", "Cliente"], 
+  "PATCH /facturas/:id": ["Administrador"],
+  "DELETE /facturas/:id": ["Administrador"],
+  "GET /facturas": ["Administrador", "Cliente"],
+
+  "GET /videos": ["Administrador", "Cliente"],
+  "GET /videos/:id": ["Administrador", "Cliente"],
+  "POST /videos": ["Administrador"],
+  "PATCH /videos/:id": ["Administrador"],
+  "DELETE /videos/:id": ["Administrador"],
+
+  "GET /monitoreo/acciones": ["Administrador"],
+  "GET /monitoreo/errores": ["Administrador"],
+
+  "POST /interacciones/:id/likes": ["Administrador", "Cliente"],
+  "POST /interacciones/:id/comentarios": ["Administrador", "Cliente"],
+  "GET /interacciones/:id": ["Administrador", "Cliente"]
 };
