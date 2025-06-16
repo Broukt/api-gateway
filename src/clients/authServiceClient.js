8d435a76641876d428ce573e984ff869da3be5c1
Jairo Calcina <j.calcina.v@gmail.com>
2025-06-15 23:12:53 -0400

    fix: fix clients references

--- src/clients/authServiceClient.js
+++ src/clients/authServiceClient.js
@@ -1,10 +1,8 @@
 const axios = require("axios");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const { authServiceUrl } = require("../config/env");
 
 const authServiceClient = axios.create({
-  baseURL: process.env.AUTH_SERVICE_URL,
+  baseURL: authServiceUrl,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
--- src/clients/grpcClients.js
+++ src/clients/grpcClients.js
@@ -1,19 +1,17 @@
 const { loadProto } = require("../utils/loadProto.js");
 const { credentials } = require("@grpc/grpc-js");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const { usersServiceUrl, playlistsServiceUrl } = require("../config/env.js");
 
 const loadClients = (app) => {
   const usersProto = loadProto("users");
-  app.locals.usersClient = new usersProto.UserService(
-    process.env.USERS_SERVICE_URL,
+  app.locals.usersClient = new usersProto.Users(
+    usersServiceUrl,
     credentials.createInsecure()
   );
 
   const playlistsProto = loadProto("playlists");
-  app.locals.playlistsClient = new playlistsProto.PlaylistService(
-    process.env.PLAYLISTS_SERVICE_URL,
+  app.locals.playlistsClient = new playlistsProto.Playlists(
+    playlistsServiceUrl,
     credentials.createInsecure()
   );
 };
