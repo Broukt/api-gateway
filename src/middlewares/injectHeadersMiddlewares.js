3a8bc025dc16186f118907f73788b9b45e7a966b
Jairo Calcina <j.calcina.v@gmail.com>
2025-06-15 23:14:09 -0400

    fix: fix inject headers middleware

--- src/middlewares/injectHeadersMiddlewares.js
+++ src/middlewares/injectHeadersMiddlewares.js
@@ -1,29 +1,28 @@
 const axios = require("axios");
 const { authServiceUrl } = require("../config/env");
-const AppError = require("./appError");
+const AppError = require("../utils/appError");
 
 const injectHeadersHTTPMiddleware = async (req, res, next) => {
   const response = await getUserTokenInfo(req, res, next);
-  if (!response) return;
-  const { userId, userEmail, role } = response.data;
-  if (!userEmail || !role) return;
+  console.log(response);
+  if (!response) {
+    req.headers["x-user-id"] = "";
+    req.headers["x-user-email"] = "";
+    req.headers["x-user-role"] = "";
+    console.log("No user info found, headers set to empty strings");
+    return next();
+  }
+  const { userId, userEmail, role } = response;
 
   req.headers["x-user-id"] = userId;
   req.headers["x-user-email"] = userEmail;
   req.headers["x-user-role"] = role;
 
-  next();
-};
-
-const injectHeadersGRPCMiddleware = async (req, res, next) => {
-  const response = await getUserTokenInfo(req, res, next);
-  if (!response) return;
-  const { userId, userEmail, role } = response.data;
-
-  const metadata = new grpc.Metadata();
-  metadata.set("x-user-id", userId);
-  metadata.set("x-user-email", userEmail);
-  metadata.set("x-user-role", role);
+  console.log("Headers injected:", {
+    "x-user-id": userId,
+    "x-user-email": userEmail,
+    "x-user-role": role,
+  });
   next();
 };
 
@@ -35,15 +34,20 @@ const getUserTokenInfo = async (req, res, next) => {
   ) {
     token = req.headers.authorization.split(" ")[1];
   }
-  const { response } = await axios.post(`${authServiceUrl}/validate`, {
-    token,
-  });
-  if (!response) {
-    return next(
-      new AppError("Something went wrong while validating token", 500)
+  let response;
+  try {
+    // this returns the HTTP response object, does NOT throw on 2xx only
+    response = await axios.post(
+      `${authServiceUrl}/validate`,
+      { token },
+      // optional: let you handle 401 in code rather than via throw
+      { validateStatus: () => true }
     );
+  } catch (err) {
+    return next(new AppError("Auth service unavailable", 502));
   }
-  if (response.status === "fail") {
+  if (response.data.status === "fail") {
+    console.log("Auth service returned fail status:", response.data);
     return null;
   }
 
@@ -53,10 +57,13 @@ const getUserTokenInfo = async (req, res, next) => {
     role: response.data.role,
   };
 
-  return response.data.userId, response.data.userEmail, response.data.role;
+  return {
+    userId: response.data.userId,
+    userEmail: response.data.userEmail,
+    role: response.data.role,
+  };
 };
 
 module.exports = {
   injectHeadersHTTPMiddleware,
-  injectHeadersGRPCMiddleware,
 };
