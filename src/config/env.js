d60711109c2239be08efc758c5cbdb50f1c676ba
Luchardiles <luiis_rapero@hotmail.cl>
2025-07-04 23:55:47 -0400

    feat: added environment variables for remainig microservices

--- src/config/env.js
+++ src/config/env.js
@@ -6,4 +6,9 @@ module.exports = {
   authServiceUrl: process.env.AUTH_SERVICE_URL || "http://localhost:4000",
   usersServiceUrl: process.env.USERS_SERVICE_URL || "localhost:50051",
   playlistsServiceUrl: process.env.PLAYLISTS_SERVICE_URL || "localhost:50052",
+  billingServiceUrl: process.env.BILLING_SERVICE_URL || "localhost:50054",
+  videosServiceUrl: process.env.VIDEOS_SERVICE_URL || "localhost:50055",
+  monitoringServiceUrl: process.env.MONITORING_SERVICE_URL || "localhost:50056",
+  socialInteractionsServiceUrl:
+    process.env.SOCIAL_INTERACTIONS_SERVICE_URL || "localhost:50057",
 };
